ab40a1023d06e371e3290609c6c6b7f903cdffe2
levincagatay <levin.cagatay@gmx.de>
2023-03-11 10:46:54 +0100

    TDD for OrderRepository

--- src/main/java/model/order/OrderRepository.java
+++ src/main/java/model/order/OrderRepository.java
@@ -3,7 +3,10 @@ package model.order;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 @Data
 @AllArgsConstructor
@@ -12,11 +15,18 @@ public class OrderRepository {
     private Map<String,Order> orders;
 
 
-    public Order findById(String id) {
-        return null;
+    public Order findById(String id) throws NoSuchElementException {
+        Stream.of(this.orders.keySet())
+                .filter(o -> o.contains(id))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+        return this.orders.get(id);
     }
 
-    public void add(Order order) {
-
+    public void add(Order order) throws KeyAlreadyExistsException {
+        if (this.orders.containsKey(order.getId())) {
+            throw new KeyAlreadyExistsException();
+        }
+        this.orders.put(order.getId(), order);
     }
 }
--- src/test/java/model/order/OrderRepositoryTest.java
+++ src/test/java/model/order/OrderRepositoryTest.java
@@ -3,19 +3,61 @@ package model.order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepositoryTest {
 
+    private Order orderOne;
+    private Order orderTwo;
+    private Order orderThree;
+    private OrderRepository orderRepository;
+
+
     @BeforeEach
     void setUp() {
+        orderOne = new Order("1", Collections.emptyMap());
+        orderTwo = new Order("2", Collections.emptyMap());
+        orderThree = new Order("3", Collections.emptyMap());
+        orderRepository = new OrderRepository(new HashMap<>(Map.of(
+                orderOne.getId(), orderOne,
+                orderTwo.getId(), orderTwo
+        )));
     }
 
     @Test
     void findById() {
+        assertThat(orderRepository.findById(orderOne.getId()))
+                .isEqualTo(orderOne);
     }
 
     @Test
+    void findById_thrownNoSuchElementException() {
+        assertThatExceptionOfType(NoSuchElementException.class)
+                .isThrownBy(() -> orderRepository.findById("ThisID does NOT exist"));
+    }
+
+
+    @Test
     void add() {
+        orderRepository.add(orderThree);
+        assertThat(orderRepository.getOrders())
+                .containsExactlyInAnyOrderEntriesOf(Map.of(
+                        orderOne.getId(), orderOne,
+                        orderTwo.getId(), orderTwo,
+                        orderThree.getId(), orderThree
+                ));
+    }
+
+    @Test
+    void add_throwsKeyAlreadyExistsException() {
+        assertThatExceptionOfType(KeyAlreadyExistsException.class)
+                .isThrownBy(() -> orderRepository.add(new Order("2", Collections.emptyMap())));
     }
 }
